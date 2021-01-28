d979150ad4d51fea272118499b6665ac118fe5ab
Theo <tgolvet6@gmail.com>
2021-01-28 23:30:22 +0100

    commentaires

--- files/states/jeudessin.lua
+++ files/states/jeudessin.lua
@@ -1,24 +1,31 @@
 function loadJeuDessin()
-  width, height, flags = love.window.getMode( )
-  widthCraie = 1280
+  width, height, flags = love.window.getMode( ) --Taille de la fenetre
+  --Taille de l'image background craie
+  widthCraie = 1280 
   heightCraie = 720
 
   gameWidth = width*0.66
   gameHeight = height*0.66
   gamex0 = (width - gameWidth)/2
   gamey0 = (height-gameHeight)/2
-  gameCanvas = love.graphics.newCanvas(gameWidth,gameHeight)
+  --gameCanvas : Canvas qui représente la "fenetre" du mini jeu sur lequel on va dessiner le background craie
+  -- puis par dessus un rectangle noir (qu'on gommera ensuite)
+  gameCanvas = love.graphics.newCanvas(gameWidth,gameHeight) 
+  -- drawingCanvas : le canvas qui accueille le rectangle noir sur lequel on va dessiner
   drawingCanvas = love.graphics.newCanvas(gameWidth,gameHeight)
-  craieBackground = love.graphics.newImage("../assets/craieback.png")
+  craieBackground = love.graphics.newImage("craieback.png")
 
   love.graphics.setCanvas(drawingCanvas)
   love.graphics.setColor(0, 0, 0, 1)
-  love.graphics.rectangle("fill", 0,0,gameWidth,gameHeight)
+  love.graphics.rectangle("fill", 0,0,gameWidth,gameHeight) --On met le rectangle noir
   love.graphics.setCanvas()
 
 end
 
 function updateJeuDessin()
+
+  --Si quelqu'un comprend ce qui se passe quand on décommente ça
+  --
   -- if timer < 5 then
   --   love.graphics.setCanvas(drawingCanvas)
   --   love.graphics.setBlendMode("multiply", "premultiplied")
@@ -26,16 +33,17 @@ function updateJeuDessin()
   --   love.graphics.ellipse("fill", gamex0,gamey0, 1, 1)
   -- end
 
+  -- Quand on appuie sur la souris on dessine
   if love.mouse.isDown(1) then
-    love.graphics.setCanvas(drawingCanvas)
+    love.graphics.setCanvas(drawingCanvas) --On dessine sur drawingCanvas
     love.graphics.setBlendMode("multiply", "premultiplied")
-    love.graphics.setColor(0, 0, 0, 0)
-    curx = love.mouse.getX() - gamex0
+    love.graphics.setColor(0, 0, 0, 0) -- Puisqu'on veut effacer on prend une couleur avec alpha = 0
+    curx = love.mouse.getX() - gamex0 -- Position où le joueur à cliqué. Je comprends pas pk il faut retrancher la position du canvas gameCanvas
     cury = love.mouse.getY() - gamey0
-    love.graphics.ellipse("fill", curx,cury, 5, 5)
-    love.graphics.setLineWidth( 10 )
+    love.graphics.ellipse("fill", curx,cury, 5, 5) --On dessine une point assez large à cet endroit
+    love.graphics.setLineWidth( 10 ) 
     love.graphics.setLineStyle( "smooth" )
-    love.graphics.line(prevx, prevy, curx, cury)
+    love.graphics.line(prevx, prevy, curx, cury) --On dessine une ligne de même epaisseur pour relier le point où est la souris a celui où elle était avant
     prevx = curx
     prevy = cury
     love.graphics.setCanvas()
@@ -43,11 +51,11 @@ function updateJeuDessin()
 end
 
 function drawJeuDessin()
-  love.graphics.setCanvas(gameCanvas)
+  love.graphics.setCanvas(gameCanvas)-- Sur le gameCanvas on affiche d'abord le background puis drawingCanvas
   love.graphics.draw(craieBackground, 0,0, 0, 0.5, 0.5)
   --love.graphics.draw(drawingCanvas,width/4, height/4)
 	love.graphics.draw(drawingCanvas)
-	love.graphics.setCanvas()
+	love.graphics.setCanvas() --on repasse sur le canvas de base puis on affiche gameCanvas (la fenetre de jeu)
 	love.graphics.draw(gameCanvas, gamex0,gamey0)
 end
 
