091d154f0e492ffe052eb017006f01a1635c9150
tremelow <29092877+tremelow@users.noreply.github.com>
2021-01-31 16:14:50 +0100

    Fixed Windows distrib with BAT file

--- build_windows.bat
+++ build_windows.bat
@@ -1,1 +1,12 @@
-copy /b love.exe+game.love game.exe
+IF EXIST Kermesse.love GOTO mkFolder
+CD files
+ZIP -9 -r Kermesse.zip *
+RENAME Kermesse.zip Kermesse.love
+MOVE Kermesse.love ..
+CD ..
+
+:mkFolder
+MD Kermesse
+COPY /b dist\love.exe+Kermesse.love Kermesse\Kermesse.exe
+COPY dist\*.dll Kermesse
+COPY dist\license.txt Kermesse
